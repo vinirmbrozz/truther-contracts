6446ff9a2f5118aaf48da559e787ffb613a49df9
Vinícius Rodrigues <mbrozzgamer@hotmail.com>
2026-01-07 22:45:08 -0300

    changing index.js to not break ESM

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import messages from './gen/node/transaction_pb.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-// Exporta as mensagens para serem usadas como: Transaction, PredictiveAnalyzer, etc.
+// Importa o arquivo gerado pelo protoc
+const messages = require('./gen/node/transaction_pb.js');
+
+// Exporta as classes para que seu teste as encontre
 export const {
     Transaction,
     PredictiveAnalyzer,
