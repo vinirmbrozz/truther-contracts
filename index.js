7f64cc86e51be1bccd278cef33622dbc6469789a
Vinícius Rodrigues <mbrozzgamer@hotmail.com>
2026-01-07 22:04:04 -0300

    Implement a professional framework using Buf, Node (TS), Go, and Python.

--- index.js
+++
@@ -1,6 +0,0 @@
-// Importa o código gerado pelo protoc
-// Nota: O caminho deve bater com onde o GitHub Actions salvou
-import messages from "./gen/node/transaction_pb";
-
-// Exporta para que o seu require('truther-contracts') funcione
-export default messages;
